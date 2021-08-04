35af33dc0249059e844c216b8604526dc2a3f857
Nikhil Gharge <61109207+nikhilgharge4591@users.noreply.github.com>
2021-08-04 13:17:02 +0100

    Initial Commit

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    var weatherManagerObj = WeatherManager()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
+    }
+    
+    
+    @IBAction func searchButtonPressed(_ sender: Any) {
+        print(searchTextField.text)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(textField.text)
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //
+        if let cityName = searchTextField.text{
+            weatherManagerObj.fetchData(cityName:cityName)
+        }
+        
+        searchTextField.text = ""
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != ""{
+            return true
+        }else{
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+
+}
+
---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Nikhil Gharge on 21/07/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=15bccef64933b014ecd8c5bc58f907d0&units=metric"
+    
+    func fetchData(cityName:String){
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        
+        //1. Create a URL
+        if let url = URL(string:urlString){
+            //2. Create the session a task
+            let session = URLSession(configuration:.default)
+            
+            //3.Given the session a task
+            session.dataTask(with: url) { (data, response, error) in
+                if error == nil{
+                    if let safeData = data{
+                        let dataString = String(data:safeData, encoding:.utf8)
+                        print(dataString)
+                    }
+                }
+            }
+        }
+    }
+}
